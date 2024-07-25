a1de5fec4320fb7b59a204dd48ec1937f7aba70b
Ivan <vany4golos@yandex.ru>
2024-07-25 20:53:18 +0300

    feat: Добавил компонент Onboarding

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,18 @@
 import { RouteProps } from 'react-router-dom';
 import { MainPage } from 'pages/MainPage';
 import { TestPage } from 'pages/TestPage';
+import { Onboarding } from '../../../widgets/Onboarding/index'
 
 export enum AppRoutes {
   MAIN = 'main',
   TEST = 'test',
+  ONBOARDING = 'test2'
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: '/',
   [AppRoutes.TEST]: '/test',
+  [AppRoutes.ONBOARDING]: '/test2',
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -21,4 +24,8 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
     path: RoutePath.test,
     element: <TestPage />,
   },
+  [AppRoutes.ONBOARDING]: {
+    path: RoutePath.test2,
+    element: <Onboarding /> 
+  }
 };

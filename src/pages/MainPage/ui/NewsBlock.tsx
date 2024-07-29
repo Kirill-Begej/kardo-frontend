40af026ba32ba4be24492d886c39097a967022e9
Ivan <vany4golos@yandex.ru>
2024-07-29 23:38:08 +0300

    fix: Верстка

--- src/pages/MainPage/ui/NewsBlock.tsx
+++ src/pages/MainPage/ui/NewsBlock.tsx
@@ -7,11 +7,46 @@ export const NewsBlock = () => {
     <Section pixelPerfect={false}>
       <Titles isLink={true} name='Новости' />
         <div className={styles.block}>
-          <div className={styles.slide}></div>
-          <div className={styles.slide}></div>
-          <div className={styles.slide}></div>
-          <div className={styles.slide}></div>
-          <div className={styles.slide}></div>
+          <div className={styles.slide}>
+            <div className={styles.textSubtitle}>
+              <p className={styles.name}>Региональный этап: Кировская область</p>
+            </div>
+            <div className={styles.date}>
+              <p className={styles.day}>10 июля</p>
+            </div>
+          </div>
+          <div className={styles.slide}>
+            <div className={styles.textSubtitle}>
+              <p className={styles.name}>Региональный этап: Кировская область</p>
+            </div>
+            <div className={styles.date}>
+              <p className={styles.day}>10 июля</p>
+            </div>
+          </div>
+          <div className={styles.slide}>
+            <div className={styles.textSubtitle}>
+              <p className={styles.name}>Региональный этап: Кировская область</p>
+            </div>
+            <div className={styles.date}>
+              <p className={styles.day}>10 июля</p>
+            </div>
+          </div>
+          <div className={styles.slide}>
+            <div className={styles.textSubtitle}>
+              <p className={styles.name}>Региональный этап: Кировская область</p>
+            </div>
+            <div className={styles.date}>
+              <p className={styles.day}>10 июля</p>
+            </div>
+          </div>
+          <div className={styles.slide}>
+            <div className={styles.textSubtitle}>
+              <p className={styles.name}>Региональный этап: Кировская область</p>
+            </div>
+            <div className={styles.date}>
+              <p className={styles.day}>10 июля</p>
+            </div>
+          </div>
         </div>
     </Section>
   );
